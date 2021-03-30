8c7cdec8858fc44de29b15be550e6cf219557fe8
w0nka <william@tractr.net>
2021-03-30 18:35:27 -0400

    placing new parts

--- soundlab-public/src/app/app.component.ts
+++ soundlab-public/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
     drumkit: `${getGithubPagesRootFolderPrefix()}/assets/instruments/drumkit/config.json`,
   };
 
-  hideSequencer = false;
+  hideSequencer = true;
 
   readonly preloadProgress$ = this.preloadService.progress;
 
